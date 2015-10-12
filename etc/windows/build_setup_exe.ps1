2ba171e169f1aae13771faeb0d3bb694e527ca29
Stefan Scherer <scherer_stefan@icloud.com>
2015-10-12 18:19:04 +0200

    Start building setup.exe with AppVeyor

---
+++ etc/windows/build_setup_exe.ps1
@@ -0,0 +1,15 @@
+$dir = C:\SourceCode\SyncThing\Binaries
+New-Item -Path $dir -Type Directory
+
+wget https://github.com/kohsuke/winsw/releases/download/1.17-beta.2/winsw.exe -OutFile $dir\syncthingservice.exe
+
+Copy-Item AUTHORS -destination $dir\AUTHORS.txt
+Copy-Item LICENSE -destination $dir\LICENSE.txt
+Copy-Item README.md -destination $dir\README.txt
+
+wget http://docs.syncthing.net/pdf/FAQ.pdf -OutFile $dir\FAQ.pdf
+wget http://docs.syncthing.net/pdf/Getting-Started.pdf -OutFile $dir\Getting-Started.pdf
+
+Copy-Item etc\windows\syncthingservice.xml -destination $dir\syncthingservice.xml
+
+nsis etc\windows\SyncThingSetup.nsi
